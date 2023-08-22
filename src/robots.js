413694d14d9d6b36bd11493a722c19bb61359884
ErinBejtaa <erinbejta@gmail.com>
2023-08-22 18:16:21 +0200

    initial commit

---
+++ src/robots.js
@@ -0,0 +1,38 @@
+export const robots = [
+{
+	id: 1,
+    name: "Leanne Graham",
+    username: "Bret",
+    email: "sincere@april.biz"
+},
+{
+	id: 2,
+    name: "Leanne Graham2",
+    username: "Bret",
+    email: "sincere@april.biz"
+},
+{
+	id: 3,
+    name: "Leanne Graham3",
+    username: "Bret",
+    email: "sincere@april.biz"
+},
+{
+	id: 4,
+    name: "Leanne Graham4",
+    username: "Bret",
+    email: "sincere@april.biz"
+},
+{
+	id: 5,
+    name: "Leanne Graham5",
+    username: "Bret",
+    email: "sincere@april.biz"
+},
+{
+	id: 6,
+    name: "Leanne Graham6",
+    username: "Bret",
+    email: "sincere@april.biz"
+},
+]
